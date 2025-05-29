221abf27a42781598c66b53ee0cc5a1451f1b194
Wen Taichi <daleth90@gmail.com>
2025-05-29 22:41:49 +0800

    quartz: set locale to zh-TW

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-TW",
     baseUrl: "wentaichi.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
