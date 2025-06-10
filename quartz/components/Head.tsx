84c567dce34783fea511c104ab9cacb20f215364
Wen Taichi <daleth90@gmail.com>
2025-06-11 03:02:56 +0800

    quartz: fix canonical for index

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,7 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-		<link rel="canonical" href={socialUrl} />
+		<link rel="canonical" href={fileData.slug === "index" ? url.toString() : socialUrl} />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
